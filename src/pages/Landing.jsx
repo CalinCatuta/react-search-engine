5193959df0c0b2c5c13edf2969f5cd02ad252ca1
Calin Catuta <calinut123@yahoo.co.uk>
2024-04-03 14:05:04 +0300

    display numbers of jobs/company in landing page.

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,15 @@
+// components
 import Search from "../components/Search";
-
+// redux
+import { useSelector } from "react-redux";
+// state
 const Landing = () => {
+  const { totalJobs, totalCompany } = useSelector((state) => state.jobs);
   return (
     <div>
+      <h3>
+        Avem {totalJobs} de oportunități în România de la {totalCompany} firme
+      </h3>
       <Search />
     </div>
   );
